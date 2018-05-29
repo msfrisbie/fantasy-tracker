bf214544607d1b417e2d3b9d390a6c1ba8e86549
Matt Frisbie <msfrisbie@gmail.com>
2018-05-29 16:16:44 -0700

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,23 +1,35 @@
-const tweetContainer = document.querySelector('#tweetcontainer');
+(() => {
+  const JSON_PATH = './tweets.json';
 
-fetch('./tweets.json').then((response) => {
-  const tweetPromise = response.json();
+  class TrackerApp {
+    constructor() {
+      this.tweetContainer = document.querySelector('#tweetcontainer');
 
-  tweetPromise.then((tweets) => {
-    for (const tweet of tweets) {
-      const tr = document.createElement('tr'),
-            idTh = document.createElement('th'),
-            textTd = document.createElement('td');
-
-      idTh.setAttribute('scope', 'row');
-
-      idTh.innerHTML = tweet.id_str;
-      textTd.innerHTML = tweet.text;
-
-      tr.appendChild(idTh);
-      tr.appendChild(textTd);
-
-      tweetContainer.appendChild(tr);
+      this.init();
     }
-  });
-});
+
+    init() {
+      fetch(JSON_PATH).then((response) => {
+        response.json().then((tweets) => {
+          for (const tweet of tweets) {
+            const tr = document.createElement('tr'),
+                  idTh = document.createElement('th'),
+                  textTd = document.createElement('td');
+
+            idTh.setAttribute('scope', 'row');
+
+            idTh.innerHTML = tweet.id_str;
+            textTd.innerHTML = tweet.text;
+
+            tr.appendChild(idTh);
+            tr.appendChild(textTd);
+
+            this.tweetContainer.appendChild(tr);
+          }
+        });
+      });
+    }
+  }
+
+  new TrackerApp();
+})();
