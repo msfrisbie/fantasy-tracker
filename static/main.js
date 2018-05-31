8d56af2b5cbaddb5e0c5234addc1daf51dbacebb
Matt Frisbie <msfrisbie@gmail.com>
2018-05-31 13:05:20 -0700

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,7 +1,8 @@
 (() => {
   const JSON_PATH = './tweets.json';
   const TWEET_CONTAINER_SELECTOR = '#tweetcontainer';
-
+  const PLAYER_SELECTION_CONTAINER_SELECTOR = '#selectioncontainer';
+  
   class Matcher {
     constructor(identifier, re) {
       this.identifier = identifier;
@@ -16,11 +17,51 @@
   class TrackerApp {
     constructor() {
       this.tweetContainer = document.querySelector(TWEET_CONTAINER_SELECTOR);
+      this.playerSelectionContainer = document.querySelector(PLAYER_SELECTION_CONTAINER_SELECTOR);
 
+      this.navbarLinks = document.querySelectorAll('[data-link]');
+      this.views = document.querySelectorAll('[data-view]');
+      
+      this.selections = [
+        {
+          memberName: 'Patrick Hamann',
+          playerName: 'Zaza Pachulia',
+          tiebreaker: 225
+        }
+      ];
+      
       this.init();
     }
 
     init() {
+      // Initialize navbar link buttons
+      for (const navbarLink of this.navbarLinks) {
+        navbarLink.addEventListener('click', (e) => {
+          this.showView(e.target.getAttribute('data-link'));
+        });
+      }
+      
+      // Initialize player selections
+      for (const memberSelection of this.memberSelections) {
+            const tr = document.createElement('tr'),
+                  memberTh = document.createElement('th'),
+                  playerNameTd = document.createElement('td'),
+                  tiebreakerTd = document.createElement('td');
+        
+        memberTh.setAttribute('scope', 'row');
+        
+        memberTh.innerHTML = memberSelection.memberName;
+        playerNameTd.innerHTML = memberSelection.playerName;
+        tiebreakerTd = memberSelection.tiebreaker;
+        
+        tr.appendChild(memberTh);
+        tr.appendChild(playerNameTd);
+        tr.appendChild(tiebreakerTd);
+        
+        this.playerSelectionContainer.appendChild(tr);
+      }
+      
+      // Initialize trump tweet list
       fetch(JSON_PATH).then((response) => {
         response.json().then((tweets) => {
           for (const tweet of tweets) {
@@ -40,6 +81,12 @@
           }
         });
       });
+    }
+      
+    showView(viewId) {
+      for (const view of this.views) {
+        view.style.display = view.getAttribute('data-view') === viewId ? 'block' : 'none';
+      }
     }
   }
 
